7c3a50fdb907a5f56c29cc354af4c346bbf9ddf1
Pavel Bely <poul-white@yandex.ru>
2020-03-01 23:26:11 +0300

    add idling hack

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,22 @@
 import { createServer } from 'http';
 import { logApp } from './logger/withLogger';
+import { compose } from './compose';
+import { api } from './request/api';
 
-export function initServer() {
-  createServer(function (request, response) {
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.end('With Love From @pabelebly', 'utf-8');
-  }).listen(process.env.PORT);
-  logApp.info(`Server running at http://127.0.0.1:${process.env.PORT}/`);
-}
+export const initServer = compose(
+  function initServer() {
+    createServer(function (request, response) {
+      response.writeHead(200, { 'Content-Type': 'text/html' });
+      response.end('With Love From @pabelebly', 'utf-8');
+    }).listen(process.env.PORT);
+    logApp.info(`Server running at port ${process.env.PORT}`);
+  },
+  function startKeepAlive() {
+    const IDLING_HACK = 20 * 60 * 1000; // load every 20 minutes
+
+    setInterval(
+      () => api.get(process.env.URL),
+      IDLING_HACK
+    );
+  }
+)
