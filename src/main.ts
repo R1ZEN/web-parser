4dce3a4a08049e2c29df70c290b48942095f563e
Pavel Bely <poul-white@yandex.ru>
2020-03-01 16:50:13 +0300

    add server

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+import { Server } from 'http';
 import { onlinerRentStrategy } from './strategy/onliner/onlinerRentStrategy';
 import { withLogger } from './logger/withLogger';
 import { initDatabase } from './database/mongodb';
 import { metroDatasetStrategy } from './strategy/metro/metroDatasetStrategy';
 import { transportDatasetStrategy } from './strategy/transport/transportDatasetStrategy';
 import { startCronJob } from './cronJob';
+
+// Server for heroku
+new Server().listen(process.env.PORT);
 
 export const main = withLogger(
   async function main() {
